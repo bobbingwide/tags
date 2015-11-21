48b4b03550d05ea72ac21bbbe5752b4e88854a7e
Herb Miller <herb@bobbingwide.com>
2015-11-21 14:06:39 +0000

    Set body and created/changed for each player. Correctly map player nid

--- includes/class-td2w-players.php
+++ includes/class-td2w-players.php
@@ -1,7 +1,9 @@
 <?php // (C) Copyright Bobbing Wide 2015
 
 /**
- * Implement the mapping for players we're migrating 
+ * Implement the mapping for players we're migrating
+ *
+ * Note: It's the player node ID not the UID that we map to the new player post ID 
  *
  */
 class TD2W_players {
@@ -13,6 +15,10 @@ class TD2W_players {
 	public $users;
 	public $terms;
 	public $files; 
+	
+	private $content;
+	private $created;
+	private $changed;
 	
 	
 	function __construct( $users, $terms, $files ) {
@@ -28,9 +34,9 @@ class TD2W_players {
 	
 	function load_players() { 
     global $wpdb;
-		$request =  "select n.nid, n.uid, n.title, t.field_woods_value, t.field_irons_value, t.field_putter_value, t.field_ball_value, t.field_handicap_value";
-		$request .= " from content_type_player t, node n  ";
-		$request .= " where t.nid = n.nid and type = 'player'";
+		$request =  "select n.nid, n.uid, n.title, n.created, n.changed, r.body, t.field_woods_value, t.field_irons_value, t.field_putter_value, t.field_ball_value, t.field_handicap_value";
+		$request .= " from content_type_player t, node n, node_revisions r  ";
+		$request .= " where t.nid = n.nid and type = 'player' and n.nid = r.nid";
 		$request .= " order by uid";
 		$results = $wpdb->get_results( $request );
 	 	$this->results = $results;
@@ -43,15 +49,16 @@ class TD2W_players {
 	
 	function process_players() {
 		foreach ( $this->results as $result ) {
+			$this->content( $result );
 			$id = $this->load_player( $result );
 			if ( !$id ) {
 				$id = $this->create_player( $result );
 			} else {
-				//$this->update_course( $result, $id );
+				$this->update_player( $result, $id );
 			}
 			$this->update_terms( $result, $id );
 			$this->set_featured_image( $id, $result );
-			$this->players[ $result->uid ] = $id;
+			$this->players[ $result->nid ] = $id;
 			
 		}
 	}
@@ -61,6 +68,18 @@ class TD2W_players {
 		return( $mapped );
 	}
 	
+	function content( $result ) {
+		$content = $result->body;
+		$content .= " [bw_fields featured]<!--more-->";
+		$content .= $result->field_woods_value;
+		$content .= $result->field_irons_value;
+		$content .= $result->field_putter_value;
+		$content .= $result->field_ball_value;
+		$content .= "[bw_fields]";
+		$this->content = $content;
+		$this->created = bw_format_date( $result->created, "Y-m-d H:i:s" );
+		$this->changed = bw_format_date( $result->changed, "Y-m-d H:i:s" );
+	}
 	
 	function load_player( $result ) {
 	
@@ -68,8 +87,8 @@ class TD2W_players {
 	
 		$atts = array( "post_type" => "player"
 								 , "post_parent" => 0
-								 , "meta_key" => "_uid"
-								 , "meta_value" => $result->uid
+								 , "meta_key" => "_nid"
+								 , "meta_value" => $result->nid
 								 );
 		$posts = bw_get_posts( $atts );
 		if ( $posts ) {
@@ -82,18 +101,15 @@ class TD2W_players {
 	
 	
 	function create_player( $result ) {
-		$content = "[bw_fields featured]<!--more-->";
-		$content .= $result->field_woods_value;
-		$content .= $result->field_irons_value;
-		$content .= $result->field_putter_value;
-		$content .= $result->field_ball_value;
-		$content .= "[bw_fields]";
 		
 		$post = array( "post_type" => "player"
 								 , "post_title" => $result->title
 								 , "post_name" => $result->title
-								 , "post_content" => $content
+								 , "post_content" => $this->content
 								 , "post_status" => "publish"
+								 , "post_date" => $this->created
+								 , "post_date_gmt" => $this->created
+								 , "post_modified_date"  => $this->changed
 								 );
 		$_POST['_user'] = $this->users->map( $result->uid );
 		$_POST['_handicap'] = $result->field_handicap_value;
@@ -102,6 +118,22 @@ class TD2W_players {
 		$id = wp_insert_post( $post );
 		return( $id );
 								
+	
+	}
+	
+	function update_player( $result, $id ) {
+		$post = array( "ID" => $id
+								 , "post_content" => $this->content
+								 , "post_date" => $this->created
+								 , "post_date_gmt" => $this->created
+								 , "post_modified_date"  => $this->changed
+								 );
+		$_POST['_user'] = $this->users->map( $result->uid );
+		$_POST['_handicap'] = $result->field_handicap_value;
+		$_POST['_uid'] = $result->uid;
+		$_POST['_nid'] = $result->nid;
+		$id = wp_update_post( $post );
+	
 	
 	}
 	
