8056bb4d8c66b6bbb3ec843897e4c9ff6ff3879b
Herb Miller <herb@bobbingwide.com>
2015-11-27 12:52:30 +0000

    Logic for real results and new_rows()

--- admin/class-tags-result.php
+++ admin/class-tags-result.php
@@ -17,6 +17,8 @@ class TAGS_result {
 	public $name;
 	private $post;
 	
+	private $new_row;
+	
 	/**
 	 * Reference back to the TAGS_results classs
 	 */										
@@ -24,14 +26,19 @@ class TAGS_result {
 	
 	function __construct( $post, $results ) {
 		$this->results = $results;
-		$ID = $post->ID;
-		$this->ID = $ID;
-		$this->event = get_post_meta( $ID, "_event", true );
-		$this->player = get_post_meta( $ID, "_player", true );
-		$this->result_type = wp_get_post_terms( $ID, "result_type", array( "fields" => "ids" ) );
-		//print_r( $this->result_type );
-		$this->details = get_post_meta( $ID, "_details", true );
-		$this->post = $post;
+		if ( $post ) {
+			$ID = $post->ID;
+			$this->ID = $ID;
+			$this->event = get_post_meta( $ID, "_event", true );
+			$this->player = get_post_meta( $ID, "_player", true );
+			$this->result_type = wp_get_post_terms( $ID, "result_type", array( "fields" => "ids" ) );
+			//print_r( $this->result_type );
+			$this->details = get_post_meta( $ID, "_details", true );
+			$this->post = $post;
+		} else {
+			$this->ID = 0;
+			
+		}
 	}
 	
 	
@@ -39,17 +46,30 @@ class TAGS_result {
 		$terms = get_terms( "result_type", array( "hide_empty" => false, "order" => "ASC" ) );
 		$term_array = bw_term_array( $terms );
 		//$term_id = get
-		return( iselect( "result_type", $this->result_type[0], array( '#options' => $term_array ) ) ); 
+		$name = $this->name( "result_type" );
+		$args = array( '#options' => $term_array );
+		if ( !$this->ID ) {
+			$args['#optional'] = true;
+		}
+		return( iselect( $name, $this->result_type[0], $args ) ); 
 	}
 	
 	/**
 	 * Return the field name taking into account the result ID
 	 */
-	function name( $name ) {
-		$name = $name;
-		$name .= "[";
-		$name .= $this->ID;
-		$name .= "]";
+	function name( $field ) {
+		if ( $this->ID ) {
+			$name = $field;
+			$name .= "[";
+			$name .= $this->ID;
+			$name .= "]";
+		} else {
+			$name = "_";
+			$name .= $field;
+			$name .= "[";
+			$name .= $this->new_row;
+			$name .= "]";
+		}
 		return( $name );
 	}	
 	
@@ -64,13 +84,14 @@ class TAGS_result {
 	}
 	
 	function details() {
-		$name = $this->name( "_details" );
+		$name = $this->name( "details" );
 		$details = itext( $name, 80, $this->details );
 		return( $details );
 	}
 	
 	
 	function ID() {
+	
 		return( $this->ID );		
 	}
 	
@@ -96,19 +117,30 @@ class TAGS_result {
 		return( $checked );
 	}
 	
+	/**
+	 * Display the details
+	 */
 	function display() {
 		$row = array();
 		$row[] = $this->ID();
 		$row[] = $this->event();
-		$row[] = $this->player();
 		$row[] = $this->result_type();
+		$row[] = $this->player();
 		$row[] = $this->details();
-		$this->result_type();
+		//$this->result_type();
 		//$row[] = $this->playing_yes();
 		//$row[] = $this->playing_no();
 		//$row[] = $this->playing_tbc();
 		bw_tablerow( $row );
 		
 	}
+	
+	function new_rows( $rows ) {
+		for ( $this->new_row=1; $this->new_row <= $rows ; $this->new_row++ ) {
+			$this->display();
+		}
+	}
+		
+		
 	
 }
